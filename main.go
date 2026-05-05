77aafaf5644013214287426eff136c797f7be77e
george-593 <63100333+george-593@users.noreply.github.com>
2026-05-05 12:19:32 +0100

    Include release date in show search and populat episode list for results

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"show-notifier/tvmaze"
 	"strconv"
+	"strings"
 )
 
 func searchShow(scanner *bufio.Scanner) (tvmaze.Show, error) {
@@ -29,8 +30,9 @@ func searchShow(scanner *bufio.Scanner) (tvmaze.Show, error) {
 	fmt.Println("Got " + strconv.Itoa(len(res)) + " results.")
 	for i := 0; i < len(res); i++ {
 		var show tvmaze.Show = res[i].Show
+		date := "(" + strings.Split(show.Premiered, "-")[0] + ")"
 
-		fmt.Printf("Are you searching for %s? (y/n) ", show.Name)
+		fmt.Printf("Are you searching for %s %s? (y/n) ", show.Name, date)
 
 		var answer string
 		scanner.Scan()
@@ -48,6 +50,12 @@ func searchShow(scanner *bufio.Scanner) (tvmaze.Show, error) {
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	search, err := searchShow(scanner)
+
+	if err != nil {
+		panic(err)
+	}
+
+	search.Episodes, err = tvmaze.FetchEpisodes(search.ID)
 
 	if err != nil {
 		panic(err)
